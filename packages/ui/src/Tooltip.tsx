ec90b8a4b1318d61c770d65a711f8844309ff0c5
VK_MSM <vitali.kaniushok@brightforce.pl>
2026-04-06 14:17:10 +0200

    Add Tooltip component with styles and functionality; update package dependencies

---
+++ packages/ui/src/Tooltip.tsx
@@ -0,0 +1,25 @@
+import { forwardRef, ComponentRef, ComponentPropsWithoutRef } from "react";
+import * as TooltipPrimitive from "@radix-ui/react-tooltip";
+import clsx from "clsx";
+
+import styles from "./Tooltip.module.scss";
+
+const TooltipProvider = TooltipPrimitive.Provider;
+const Tooltip = TooltipPrimitive.Root;
+const TooltipTrigger = TooltipPrimitive.Trigger;
+
+const TooltipContent = forwardRef<
+  ComponentRef<typeof TooltipPrimitive.Content>,
+  ComponentPropsWithoutRef<typeof TooltipPrimitive.Content>
+>(({ className, sideOffset = 4, ...props }, ref) => (
+  <TooltipPrimitive.Content
+    ref={ref}
+    sideOffset={sideOffset}
+    className={clsx(styles.content, className)}
+    {...props}
+  />
+));
+
+TooltipContent.displayName = TooltipPrimitive.Content.displayName;
+
+export { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider };
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,3 +1,9 @@
 export { GlowText } from "./GlowText";
 export { ContainerNarrow } from "./ContainerNarrow";
 export { Button } from "./Button";
+export {
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+  TooltipProvider,
+} from "./Tooltip";
