700594f82004291728abcce8ed9aee4c7235aab6
VK_MSM <vitali.kaniushok@brightforce.pl>
2026-04-06 14:17:52 +0200

    Refactor SectionWrapper component to include title and overTitle props; enhance styling with new SCSS classes

--- apps/portfolio/src/components/ui/SectionWrapper.tsx
+++ apps/portfolio/src/components/ui/SectionWrapper.tsx
@@ -1,22 +1,41 @@
 import { type FC, type ReactNode } from "react";
 import clsx from "clsx";
+import { motion } from "framer-motion";
 
-import style from "./SectionWrapper.module.scss";
+import { ContainerNarrow } from "@frontfolio/ui";
+
+import styles from "./SectionWrapper.module.scss";
 
 interface SectionWrapperProps {
   className?: string;
   id: string;
+  title: string;
+  overTitle: string;
   children: ReactNode;
 }
 
 export const SectionWrapper: FC<SectionWrapperProps> = ({
   className,
   id,
+  title,
+  overTitle,
   children,
 }) => {
   return (
-    <section id={id} className={clsx(style.section, className)}>
-      {children}
+    <section id={id} className={clsx(styles.section, className)}>
+      <ContainerNarrow>
+        <motion.div
+          initial={{ y: 20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.5 }}
+          className={styles["title-wrapper"]}
+        >
+          <p className={styles["over-title"]}>{overTitle}</p>
+          <h2 className={styles.title}>{title}</h2>
+        </motion.div>
+        {children}
+      </ContainerNarrow>
     </section>
   );
 };
