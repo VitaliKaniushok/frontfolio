424ddb8ca59385a62c6928a3a048a726ac590067
VK_MSM <vitali.kaniushok@brightforce.pl>
2026-04-24 00:03:50 +0200

    fix: update @devfolio/i18n dependency to eager loading for improved performance (this is fix after build docker container)

--- packages/config/federation/shared.ts
+++ packages/config/federation/shared.ts
@@ -9,7 +9,7 @@ export const sharedDeps: Record<string, SharedDep> = {
   "react-dom": { singleton: true, requiredVersion: false, eager: true },
   i18next: { singleton: true, requiredVersion: false },
   "react-i18next": { singleton: true, requiredVersion: false },
-  "@devfolio/i18n": { singleton: true, requiredVersion: false },
+  "@devfolio/i18n": { singleton: true, requiredVersion: false, eager: true },
   "next/router": { singleton: true, requiredVersion: false },
   "next/navigation": { singleton: true, requiredVersion: false },
   "next/link": { singleton: true, requiredVersion: false },
